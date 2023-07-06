f39ad85553a5158fb3f14839259115b50988a96b
littlerain2310 <tuanxinghiepdongda@gmail.com>
2023-07-06 18:37:51 +1000

    fix the converter

--- tools/convert_labeme_label.py
+++ tools/convert_labeme_label.py
@@ -4,15 +4,30 @@ import argparse
 import glob
 import os
 import os.path as osp
-import sys
 
-import imgviz
 import numpy as np
-import matplotlib.pyplot as plt
+import PIL.Image
 
 import labelme
 
+def lblsave(filename, lbl):
+    import imgviz
 
+    if osp.splitext(filename)[1] != ".png":
+        filename += ".png"
+    # Assume label ranses [-1, 254] for int32,
+    # and [0, 255] for uint8 as VOC.
+    if lbl.min() >= -1 and lbl.max() < 255:
+        lbl_pil = PIL.Image.fromarray(lbl.astype(np.uint8), mode="P")
+        colormap = imgviz.label_colormap()
+        lbl_pil.putpalette(colormap.flatten())
+        lbl_pil = lbl_pil.convert("RGB")
+        lbl_pil.save(filename)
+    else:
+        raise ValueError(
+            "[%s] Cannot save the pixel-wise class label as PNG. "
+            "Please consider using the .npy format." % filename
+        )
 def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
@@ -26,7 +41,6 @@ def main():
 
     
     
-
     class_names = []
     class_name_to_id = {}
     for i, line in enumerate(open(args.labels).readlines()):
@@ -43,6 +57,9 @@ def main():
     print('class_names:', class_names)
     class_name_to_id['other'] = 0
 
+    #create a new dir for converted data
+    output_dir = osp.join(os.path.dirname(args.label_dir),'labels_converted')
+    os.makedirs(output_dir,exist_ok = True)
     for filename in glob.glob(osp.join(args.label_dir, '*.json')):
         print('Generating dataset from:', filename)
 
@@ -56,4 +73,8 @@ def main():
             shapes=label_file.shapes,
             label_name_to_value=class_name_to_id,
         )
-        labelme.utils.lblsave(f'labels_converted/{base}', lbl)
+        
+        lblsave(f'{output_dir}/{base}', lbl)
+        
+if __name__ == '__main__':
+    main()
